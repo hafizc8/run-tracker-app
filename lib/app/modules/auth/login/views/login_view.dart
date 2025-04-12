62fee8b82bbbb7b2f6948930ba25910a994bcab8
Hafiz Mutakin <hafizc8@gmail.com>
2025-04-12 13:52:41 +0700

    done - login - ui

---
+++ lib/app/modules/auth/login/views/login_view.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:get/get.dart';
+import 'package:zest_mobile/app/routes/app_routes.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 24),
+            const Placeholder(
+              fallbackHeight: 200,
+              fallbackWidth: 200,
+              child: Text('Logo'),
+            ),
+            const SizedBox(height: 24),
+            Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 24),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Email',
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      const SizedBox(height: 12),
+                      TextFormField(
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your email',
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Password',
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      const SizedBox(height: 12),
+                      TextFormField(
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your password',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () => Get.toNamed(AppRoutes.forgotPassword),
+                child: Text(
+                  'Forgot Password?',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'or login with',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/icons/ic_google.svg', width: 36),
+                const SizedBox(width: 20),
+                SvgPicture.asset('assets/icons/ic_fb.svg', width: 36),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Don\'t have account?',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 4),
+            TextButton(
+              onPressed: () => Get.toNamed(AppRoutes.register),
+              child: Text(
+                'Sign Up',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,42 +1,34 @@
 import 'package:get/get.dart';
-import 'package:zest_mobile/app/modules/login/bindings/login_binding.dart';
-import 'package:zest_mobile/app/modules/login/views/login_view.dart';
-import 'package:zest_mobile/app/modules/register/bindings/register_binding.dart';
-import 'package:zest_mobile/app/modules/register/bindings/register_verify_email_success_binding.dart';
-import 'package:zest_mobile/app/modules/register/views/register_view.dart';
-import 'package:zest_mobile/app/modules/register/bindings/register_verify_email_binding.dart';
-import 'package:zest_mobile/app/modules/register/views/register_verify_email_view.dart';
-import 'package:zest_mobile/app/modules/register/views/register_verify_email_success.dart';
+import 'package:zest_mobile/app/modules/auth/forgot_password/bindings/forgot_password_binding.dart';
+import 'package:zest_mobile/app/modules/auth/forgot_password/views/forgot_password_email_sent_view.dart';
+import 'package:zest_mobile/app/modules/auth/forgot_password/views/forgot_password_password_updated_view.dart';
+import 'package:zest_mobile/app/modules/auth/forgot_password/views/forgot_password_set_new_password_view.dart';
+import 'package:zest_mobile/app/modules/auth/forgot_password/views/forgot_password_view.dart';
+import 'package:zest_mobile/app/modules/auth/login/bindings/login_binding.dart';
+import 'package:zest_mobile/app/modules/auth/login/views/login_view.dart';
+import 'package:zest_mobile/app/modules/auth/register/bindings/register_binding.dart';
+import 'package:zest_mobile/app/modules/auth/register/bindings/register_verify_email_success_binding.dart';
+import 'package:zest_mobile/app/modules/auth/register/views/register_view.dart';
+import 'package:zest_mobile/app/modules/auth/register/bindings/register_verify_email_binding.dart';
+import 'package:zest_mobile/app/modules/auth/register/views/register_verify_email_view.dart';
+import 'package:zest_mobile/app/modules/auth/register/views/register_verify_email_success.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import 'app_routes.dart';
 
 class AppPages {
   static final pages = [
-    GetPage(
-      name: AppRoutes.home,
-      page: () => const HomeView(),
-      binding: HomeBinding(),
-    ),
-    GetPage(
-      name: AppRoutes.register,
-      page: () => const RegisterView(),
-      binding: RegisterBinding(),
-    ),
-    GetPage(
-      name: AppRoutes.registerVerifyEmail,
-      page: () => const RegisterVerifyEmailView(),
-      binding: RegisterVerifyEmailBinding(),
-    ),
-    GetPage(
-      name: AppRoutes.registerVerifyEmailSuccess,
-      page: () => const RegisterVerifyEmailSuccessView(),
-      binding: RegisterVerifyEmailSuccessBinding(),
-    ),
-    GetPage(
-      name: AppRoutes.login,
-      page: () => const LoginView(),
-      binding: LoginBinding(),
-    ),
+    GetPage(name: AppRoutes.home, page: () => const HomeView(), binding: HomeBinding()),
+
+    // Auth
+    GetPage(name: AppRoutes.register, page: () => const RegisterView(), binding: RegisterBinding()),
+    GetPage(name: AppRoutes.registerVerifyEmail, page: () => const RegisterVerifyEmailView(), binding: RegisterVerifyEmailBinding()),
+    GetPage(name: AppRoutes.registerVerifyEmailSuccess, page: () => const RegisterVerifyEmailSuccessView(), binding: RegisterVerifyEmailSuccessBinding()),
+    
+    GetPage(name: AppRoutes.login, page: () => const LoginView(), binding: LoginBinding()),
+    GetPage(name: AppRoutes.forgotPassword, page: () => const ForgotPasswordView(), binding: ForgotPasswordBinding()),
+    GetPage(name: AppRoutes.forgotPasswordEmailSent, page: () => const ForgotPasswordEmailSentView()),
+    GetPage(name: AppRoutes.forgotPasswordSetNew, page: () => const ForgotPasswordSetNewPasswordView()),
+    GetPage(name: AppRoutes.forgotPasswordUpdated, page: () => const ForgotPasswordPasswordUpdatedView()),
   ];
 }
